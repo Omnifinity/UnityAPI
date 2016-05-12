bcd73efd199eeff074241e3e78cef6bf5169f47c
Peter Thor <peter.thor@omnifinity.se>
2016-05-12 12:17:31 +0200

    Added scene and hand controller script for enabling/disabling simulated Omnideck movement based on the position of the left handcontroller. Place the hand controller close to your hip. When grip button is pressed the position of the CameraRig will be offset 1/10th in the direction of the hand controller. Thus the user knows that their initial implementation is compatible with the Omnideck.

---
+++ Assets/Omnifinity/Scripts/OmnitrackHandController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Omnifinity.Omnitrack;
+
+public class OmnitrackHandController : MonoBehaviour
+{
+    private OmnitrackCharacterController omnitrackCharacterController;
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+    private SteamVR_TrackedObject trackedObject;
+
+    // get the most newest object index
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObject.index); } }
+    
+    private bool temporaryDisabledInteractingItemCollider = false;
+
+    public GameObject omnitrackDebugPrefab;
+    private GameObject goOmnitrackDebug;
+
+    // Use this for initialization
+    void Start()
+    {
+        // reference to the tracked object
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+
+        omnitrackCharacterController = FindObjectOfType<OmnitrackCharacterController>();
+        omnitrackCharacterController.setIsPositionEnabled(false);
+    }
+
+    void Update()
+    {
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        if (controller.GetPressDown(gripButton))
+        {
+            // flip mode
+            omnitrackCharacterController.flipIsPositionEnabled();
+            if (omnitrackCharacterController.getIsPositionEnabled())
+            {
+                goOmnitrackDebug = Instantiate(omnitrackDebugPrefab, transform.position, transform.rotation) as GameObject;
+                goOmnitrackDebug.transform.parent = transform;
+                goOmnitrackDebug.transform.Rotate(Vector3.right, 45f, Space.Self);
+                goOmnitrackDebug.transform.Translate(Vector3.up * 0.1f, Space.Self);
+
+            }
+            else
+            {
+                if (goOmnitrackDebug != null)
+                {
+                    Destroy(goOmnitrackDebug);
+                }
+            }
+        }
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+    }
+}
