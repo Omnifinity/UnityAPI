764c14af3874a0d08da3595995a9575e1aa424e4
Peter Thor <peter.thor@omnifinity.se>
2016-05-16 14:40:52 +0200

    Exposes new parameters for hand controlled Omnideck or simulated motion.

--- OmnitrackGameClient/RELEASENOTES.md
+++ OmnitrackGameClient/RELEASENOTES.md
@@ -1,6 +1,13 @@
 ## RELEASE NOTES
 **Omnitrack Game Client**
 
+## 1.15.12
+Exposes some new parameters:
+
+- usehandcontrollerasbody 0/1 (1 by default)
+- simulatetracking 0/1 (0 by default)
+- sendcircularpathtrackingdata 0/1 (0 by default)
+
 ## 1.15.9
 Initial release to public. 
 
--- OmnitrackGameClient/bin/win32/omnitrackgameclient.bat
+++ OmnitrackGameClient/bin/win32/omnitrackgameclient.bat
@@ -5,4 +5,4 @@ REM These are the minimum set of parameters required to run the software
 REM Alter the ip and port as you desire. 
 REM 
 
-".\OmnitrackGameClient.exe" --ip 192.168.176.100 --porttrackingdata 11016 --simulatetracking 1 --printfps 0
+".\OmnitrackGameClient.exe" --ip 192.168.176.100 --porttrackingdata 11016 --fps 30 --printfps 0
